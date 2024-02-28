1cfe58171fc0cb03e34e73931390d93ce9fa372b
Lukas <luk.brasiunas@gmail.com>
2024-02-28 19:53:29 +0200

    Working docker-compose with two services and their databases

--- LibraryWebService/Library/Program.cs
+++ LibraryWebService/Library/Program.cs
@@ -16,7 +16,7 @@ namespace Library
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var server = builder.Configuration["Server"] ?? "db";
+            var server = builder.Configuration["Server"] ?? "library-db";
             var port = builder.Configuration["Port"] ?? "1433";
             var database = builder.Configuration["Database"] ?? "Library";
 
